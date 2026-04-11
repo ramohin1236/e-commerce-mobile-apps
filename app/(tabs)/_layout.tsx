481a15ab287cc7b17e3bcee4efb9fc68ba94df70
ramohin1236 <mohinr26@gmail.com>
2026-04-11 11:52:07 +0600

    first commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,62 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { COLORS } from '@/constants'
+import { Ionicons } from '@expo/vector-icons'
+import { Tabs } from 'expo-router'
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+    return (
+        <Tabs>
+            <Tabs.Screen name="index" options={{
+                title: "Home", headerShown: false, tabBarActiveTintColor: COLORS.primary, tabBarInactiveTintColor: "#CDCDE0",
+                tabBarStyle: {
+                    backgroundColor: "#fff",
+                    borderTopWidth: 1,
+                    borderTopColor: "#fofofo",
+                    height: 60,
+                    paddingBottom: 10,
 
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+                },
+                tabBarIcon: ({ color, focused }) => <Ionicons name={focused ? "home" : "home-outline"} size={24} color={color} />
+            }} />
+
+            <Tabs.Screen name="cart" options={{
+                title: "Cart", headerShown: false, tabBarActiveTintColor: COLORS.primary, tabBarInactiveTintColor: "#CDCDE0",
+                tabBarStyle: {
+                    backgroundColor: "#fff",
+                    borderTopWidth: 1,
+                    borderTopColor: "#fofofo",
+                    height: 60,
+                    paddingBottom: 10,
+
+                },
+                tabBarIcon: ({ color, focused }) => <Ionicons name={focused ? "cart" : "cart-outline"} size={24} color={color} />
+            }} />
+
+            <Tabs.Screen name="favorites" options={{
+                title: "Favorites", headerShown: false, tabBarActiveTintColor: COLORS.primary,
+                tabBarStyle: {
+                    backgroundColor: "#fff",
+                    borderTopWidth: 1,
+                    borderTopColor: "#fofofo",
+                    height: 60,
+                    paddingBottom: 10,
+
+                },
+                tabBarInactiveTintColor: "#CDCDE0", tabBarIcon: ({ color, focused }) => <Ionicons name={focused ? "heart" : "heart-outline"} size={24} color={color} />
+            }} />
+
+            <Tabs.Screen name="profile" options={{
+                title: "Profile", headerShown: false, tabBarActiveTintColor: COLORS.primary,
+                tabBarStyle: {
+                    backgroundColor: "#fff",
+                    borderTopWidth: 1,
+                    borderTopColor: "#fofofo",
+                    height: 60,
+                    paddingBottom: 10,
+
+                },
+                tabBarInactiveTintColor: "#CDCDE0", tabBarIcon: ({ color, focused }) => <Ionicons name={focused ? "person" : "person-outline"} size={24} color={color} />
+            }} />
+        </Tabs>
+    )
 }
